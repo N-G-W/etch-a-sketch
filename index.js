02a53077c9d5f6a1b55c8d9365ca9558a87e7472
nitin <nitinghimire.w@gmail.com>
2025-08-11 23:38:11 +0545

    Add preliminary css

---
+++ index.js
@@ -0,0 +1,23 @@
+//create grids that are apparently divs
+//these grids will access the container div and add
+// grids to that
+//using flexbox to create grids
+//setting up a hover effect so grid divs change color
+// when mouse passes over them leaving a pixelated trail
+//through the grid like a pen
+// allow a button to ask the number of sides of new grid
+
+// 1. create a function called createBlock
+// This function should:
+// a)Take in as an input a number that defines
+// the number of sides of the sketchpad
+// b)It should also create the divs elements/nodes
+// c)It should append those nodes to the div container
+// d)Use the hover event on the div to change the color
+// of the divs
+// e)Add button that asks the number of side for new grid
+
+// Key challenges:
+// How to create a grid using flexbox
+// How to change the color of the div
+
